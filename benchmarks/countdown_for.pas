ed0163e6763c317b60ebd058c527ed392dc3eeb4
zbyti <zbyti@yahoo.com>
2020-07-25 16:12:42 +0200

    refactor

---
+++ benchmarks/countdown_for.pas
@@ -0,0 +1,44 @@
+unit countdown_for;
+
+//---------------------- INTERFACE ---------------------------------------------
+
+interface
+  {$i '../includes/interface.inc'}
+
+//---------------------- TYPES -------------------------------------------------
+
+//---------------------- CONSTANS ----------------------------------------------
+const
+  name      : string = 'countdown_for';
+
+//---------------------- VARIABLES ---------------------------------------------
+var
+  score     : word;
+
+//---------------------- IMPLEMENTATION ----------------------------------------
+
+implementation
+
+procedure run;
+var
+  za         : byte absolute $41;
+  zb         : byte absolute $42;
+  zc         : byte absolute $43;
+  zd         : byte absolute $44;
+  ze         : byte absolute $45;
+  zf         : byte absolute $46;
+  zg         : byte absolute $47;
+begin
+  for za:=1 downto 0 do
+    for zb:=9 downto 0 do
+      for zc:=9 downto 0 do
+        for zd:=9 downto 0 do
+          for ze:=9 downto 0 do
+            for zf:=9 downto 0 do
+              for zg:=9 downto 0 do;
+end;
+
+//---------------------- INITIALIZATION ----------------------------------------
+
+initialization
+end.
---
+++ benchmarks/countdown_while.pas
@@ -0,0 +1,66 @@
+unit countdown_while;
+
+//---------------------- INTERFACE ---------------------------------------------
+
+interface
+  {$i '../includes/interface.inc'}
+
+//---------------------- TYPES -------------------------------------------------
+
+//---------------------- CONSTANS ----------------------------------------------
+const
+  name      : string = 'countdown_while';
+
+//---------------------- VARIABLES ---------------------------------------------
+var
+  score     : word;
+
+//---------------------- IMPLEMENTATION ----------------------------------------
+
+implementation
+
+procedure run;
+var
+  za         : byte absolute $41;
+  zb         : byte absolute $42;
+  zc         : byte absolute $43;
+  zd         : byte absolute $44;
+  ze         : byte absolute $45;
+  zf         : byte absolute $46;
+  zg         : byte absolute $47;
+begin
+  za := 1; zb := 9; zc := 9; zd := 9;
+  ze := 9; zf := 9; zg := 9;
+  while za <> $ff do begin
+    zb := 9;
+    while zb <> $ff do begin
+      zc := 9;
+      while zc <> $ff do begin
+        zd := 9;
+        while zd <> $ff do begin
+          ze := 9;
+          while ze <> $ff do begin
+            zf := 9;
+            while zf <> $ff do begin
+              zg := 9;
+              while zg <> $ff do begin
+                dec(zg);
+              end;
+              dec(zf);
+            end;
+            dec(ze);
+          end;
+          dec(zd);
+        end;
+        dec(zc);
+      end;
+      dec(zb);
+    end;
+    dec(za);
+  end;
+end;
+
+//---------------------- INITIALIZATION ----------------------------------------
+
+initialization
+end.
