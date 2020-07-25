241e0edede3c9b2dc78fff95aff7036f24666cd6
zbyti <zbyti@yahoo.com>
2020-07-25 16:18:18 +0200

    refactor

--- benchmarks/sieve.pas
+++ benchmarks/sieve.pas
@@ -26,20 +26,20 @@ var
   n         : byte absolute $e0;
   k         : word absolute $e2;
 begin
-  score :=0;
+  score := 0;
   fillchar(flags, sizeof(flags), true);
   for n := 2 to 91 do begin
     if flags[n] then begin
       k := n shl 1;
       while k <= 8191 do begin
         flags[k] := false;
-        Inc(k,n);
+        inc(k,n);
       end;
     end;
   end;
 
   for k := 2 to 8191 do begin
-    if flags[k] then Inc(score);
+    if flags[k] then inc(score);
   end;
 end;
 
