0bfa05f4a8b982167888788735b06ee7559fbde6
zbyti <zbyti@yahoo.com>
2020-09-14 15:03:23 +0200

    refactor

--- benchmarks/matrix_t.pas
+++ benchmarks/matrix_t.pas
@@ -7,16 +7,18 @@ unit matrix_t;
 //---------------------- IMPLEMENTATION ----------------------------------------
 
 procedure benchmark;
+const
+  size = 255;
 var
   x         : byte absolute $e0;
   y         : byte absolute $e1;
   A, B      : array[0..0, 0..0] of byte;
 
 begin
-  for y := 0 to 255 do
-    for x := 0 to 255 do A[y][x] := rnd;
-  for y := 0 to 255 do
-    for x := 0 to 255 do B[x][y] := A[y][x];
+  for y := 0 to size do
+    for x := 0 to size do A[y][x] := rnd;
+  for y := 0 to size do
+    for x := 0 to size do B[x][y] := A[y][x];
 end;
 
 //---------------------- COMMON PROCEDURE --------------------------------------
